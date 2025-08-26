2c75752d4186d925f7a2a5a338780d29c7ff5d24
Tair Sangama <tairsangam@gmail.com>
2025-08-26 17:03:36 -0500

    Corregir la imágen del home

--- amazonia-viva-web/src/pages/Home.jsx
+++ amazonia-viva-web/src/pages/Home.jsx
@@ -1,11 +1,10 @@
+import React from "react";
+import Hero from "../components/Hero";
+
 function Home() {
   return (
-    <div >
-      <h2>Bienvenido a Amazonía Viva 🌎</h2>
-      <p>
-        Esta aplicación es un demo para mostrar un catálogo de especies amazónicas,
-        un mapa interactivo y recomendaciones para cuidar el planeta.
-      </p>
+    <div>
+      <Hero />
     </div>
   );
 }
