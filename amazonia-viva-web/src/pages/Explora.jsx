e467395bab8e8b44e2debfa01c370ade1fe19e8f
Tair Sangama <tairsangam@gmail.com>
2025-08-27 09:26:47 -0500

    Agregué un nuevo archivo(Explora.css). Y dí estilos al archivo(Explora.jsx)

--- amazonia-viva-web/src/pages/Explora.jsx
+++ amazonia-viva-web/src/pages/Explora.jsx
@@ -1,9 +1,10 @@
+import "./Explora.css"
 function Explora() {
   return (
     
     <section className="explora">
 
-      <div className="Explora-content">
+      <div className="explora-content">
       <h1>Catálogo de Especies 📚</h1>
       <p>Aquí encontrarás especies de flora y fauna amazónica.</p>
     </div>
